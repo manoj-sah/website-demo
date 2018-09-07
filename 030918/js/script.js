99964e4bed707bfeb132c5cfd29c0ef9585e1046
Arindam Ganguly <arindam@moonraft.com>
2018-09-07 19:10:35 +0530

    function modified

--- 030918/js/script.js
+++ 030918/js/script.js
@@ -1,16 +1,35 @@
-
 function accordion() {
     var acc = document.getElementsByClassName("accordion");
+    var arrow = document.getElementsByClassName("arrow-style");
     var i;
     for (i = 0; i < acc.length; i++) {
+        
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+        this.nextElementSibling.classList.toggle("panel-show");
     });
 }
+}
+
+function validate(){
+    var password = document.getElementById("transaction-password-input").value;
+    var allowedCharacters = /^[0-9a-zA-Z]+$/;
+    var messageDiv = document.getElementById("warnning-message");
+    messageDiv.classList.add("show")
+    if(password.length > 0){
+       if(password.match(allowedCharacters)){
+        messageDiv.classList.remove("show");
+        alert("Password accepted");
+    }
+    else{
+        
+        messageDiv.innerHTML="Please enter password which should not contain any symbols";
+    }
+    }else{
+        messageDiv.innerHTML="Please enter password which should not contain any symbols";
+       }
+}
+
+function removeMsg(){
+    document.getElementById("warnning-message").classList.remove("show");
 }

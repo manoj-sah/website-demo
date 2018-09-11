f1ce8c9b7da48f6ceb47fc0cc89d92d1a1f83f50
Arindam Ganguly <arindam@moonraft.com>
2018-09-11 17:55:06 +0530

    animation modified

--- 030918/js/script.js
+++ 030918/js/script.js
@@ -1,38 +1,46 @@
 var getHeightofDisplayBlock = function(element){
   element.style.display = 'block';
   var height = element.scrollHeight+"px";
-  console.log(height);
   element.style.display = '';
   return height;
 }
-
+let closeOpenAccordionBlock = () =>{
+  const acc = document.getElementsByClassName("accordion");
+  for (let i = 0; i < acc.length; i++) {
+          const displayBlock = acc[i].nextElementSibling.style;
+          const arrow = acc[i].childNodes[1].classList;
+          if(parseInt(displayBlock.height) > 0){
+            displayBlock.height = 0+"px";
+            arrow.remove("fa-angle-up");
+            arrow.add("fa-angle-down");
+          }
+  }
+}
 function accordion() {
-    var acc = document.getElementsByClassName("accordion");
+    const acc = document.getElementsByClassName("accordion");
     for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        var height = getHeightofDisplayBlock(this.nextElementSibling);
-        var arrow = this.childNodes[1].classList;
+        const height = getHeightofDisplayBlock(this.nextElementSibling);
+        const arrow = this.childNodes[1].classList;
         if(arrow.contains("fa-angle-up")){
             this.nextElementSibling.style.height = 0+"px";
             arrow.remove("fa-angle-up");
             arrow.add("fa-angle-down");
         }else{
+            closeOpenAccordionBlock();
             this.nextElementSibling.style.height = height;
             arrow.add("fa-angle-up");
             arrow.remove("fa-angle-down");
         }
-
         this.nextElementSibling.classList.toggle("panel-show");
-
-    });
-
-}
+      });
+    }
 }
 
 function validate(){
-    var password = document.getElementById("transaction-password-input").value;
-    var allowedCharacters = /^[0-9a-zA-Z]+$/;
-    var messageDiv = document.getElementById("warnning-message");
+    const password = document.getElementById("transaction-password-input").value;
+    const allowedCharacters = /^[0-9a-zA-Z]+$/;
+    const messageDiv = document.getElementById("warnning-message");
     messageDiv.classList.add("show")
     if(password.length > 0){
        if(password.match(allowedCharacters)){
